a00fe8c4b6ca231d78e6a61b86bace73051b912d
footoredo <footoredo@gmail.com>
2019-08-08 09:58:16 -0400

    experimenting

--- src/analysis_sec.py
+++ src/analysis_sec.py
@@ -7,7 +7,7 @@ from common.path_utils import *
 import joblib
 import numpy as np
 
-exp_name = "security_ppo_seed:5410_game:2-2-5-0.5:0.5_gs_no-reset_5e-6_constant_latest_test_every:10_test_steps:1000_network:256-4_train:10*20"
+exp_name = "security_ppo_seed:5410_game:5-2-5-0.5:0.5_gs_no-reset_5e-6_constant_latest_test_every:10_test_steps:1000_network:256-4_train:10*20"
 step = 10000
 exp_dir = join_path("../result", exp_name)
 step_dir = join_path(exp_dir, "step-{}".format(step))
@@ -45,7 +45,7 @@ def analysis_final_assessment(final_assessment, statistics):
     # sns.distplot(result)
     # plt.show()
 
-    standard = 1.05
+    standard = 1000.
 
     stupid_result = {
         "len": [],
@@ -53,7 +53,11 @@ def analysis_final_assessment(final_assessment, statistics):
         "eps": [],
         "type": []
     }
+
+    weighted_sum = [0.] * env.n_rounds
+
     for h, v in atk_result:
+        weighted_sum[len(h) - 1] += statistics.get_freq(0, get_atk_ob(h)) * v * env.n_rounds / (10. * (env.n_rounds + 1 - len(h)))
         if v > standard:
             print(h, v, statistics.get_freq(0, get_atk_ob(h)))
             stupid_result["len"].append(len(h) - 1)
@@ -61,6 +65,8 @@ def analysis_final_assessment(final_assessment, statistics):
             # stupid_result["eps"].append(v / (11 - len(h)))
             stupid_result["eps"].append(v)
             stupid_result["type"].append(h[0])
+
+    print("weighted sum:", weighted_sum)
 
     df = pd.DataFrame(data=stupid_result)
     # sns.relplot(x="freq", y="eps", hue="type", data=df)
